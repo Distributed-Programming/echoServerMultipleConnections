e530076c07d361127acac4863820c6f805f32e61
Ebbe Vang <ev@evang.dk>
2015-09-03 08:38:49 +0200

    adds project to git

---
+++ EchoServerMultipleConnections/EchoServerMultipleConnections/EchoConnection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServerMultipleConnections
+{
+
+    // this class is used as a single connection to a tcp client
+    class EchoConnection
+    {
+        private Socket socket;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private EchoServer echoServer;
+
+        public EchoConnection(Socket socket, EchoServer echoServer)
+        {
+            this.echoServer = echoServer;
+            this.socket = socket;
+          
+            NetworkStream netStr = new NetworkStream(socket);
+            reader = new StreamReader(netStr);
+            writer = new StreamWriter(netStr);
+
+            Console.WriteLine("Connected");
+        }
+
+        public void Listen()
+        {
+            while (true)
+            {
+                string receivedString = reader.ReadLine();
+                echoServer.incomingMessage(receivedString);
+            }            
+        }
+
+        public void sendMessage(String message)
+        {
+            writer.WriteLine(message);
+            writer.Flush();
+        }
+
+        public void disposeSocket()
+        {
+            writer.Close();
+            reader.Close();
+            socket.Close();
+        }
+
+    }
+}
---
+++ EchoServerMultipleConnections/EchoServerMultipleConnections/EchoServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace EchoServerMultipleConnections
+{
+    // this class spawns waits for a tcp connection and spawn a echocennection for each tcp connection
+    // server supports multiple connections and echo to everyone connected
+    // each connection is sotred in a list
+    // todo: closing connections gracefully
+
+    class EchoServer
+    {
+        // set port number and ip
+        int portNumber = 8000;
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+
+        private List<EchoConnection> connections = new List<EchoConnection>();
+
+        public EchoServer()
+        {
+            TcpListener listener = new TcpListener(ip, portNumber);
+            listener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for a conncetion - number of connections: {0}", connections.Count);
+                Socket socket = listener.AcceptSocket();
+                EchoConnection conn = new EchoConnection(socket, this);
+                connections.Add(conn);
+
+                Task.Factory.StartNew(() => conn.Listen());
+                Console.WriteLine("Connected a client");          
+            }
+        }
+
+        public void incomingMessage(string receivedString)
+        {
+            Console.WriteLine("Incoming message: {0}", receivedString);
+           foreach (EchoConnection con in connections)
+            {
+                con.sendMessage(receivedString);
+            }
+        }
+    }
+}
